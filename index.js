fb34b8c445d01106a5889560c4af8fe69ed9722e
Sibeko Siphiwe <143623793+CyberGhost808@users.noreply.github.com>
2024-07-29 11:52:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function WhatsApp(){
+    alert("Accessing WhatsApp");
+    window.open ("https://web.whatsapp.com/send?phone=27835527669", "_black")
+}
+
+function Menu(){
+    document.getElementById('Menu').style.display = "block";
+}
+function Dissapear(){
+    document.getElementById('Menu').style.display = "none";
+}
